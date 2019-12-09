81e5349b547eafb96430b38ffdb0e3f9029b6950
Unknown <Elliot2.martin@live.uwe.ac.uk>
2019-12-09 18:42:27 +0000

    UI additions

---
+++ NotGoogleGlassesUnity/Assets/Scripts/HUDObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// Contains UI elements for a tracked Object
+/// </summary>
+public class HUDObject : MonoBehaviour
+{
+    public Text object_name;
+
+    public void SetName(string name)
+    {
+        object_name.text = name;
+    }
+}
---
+++ NotGoogleGlassesUnity/Assets/Scripts/HudTracker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the HUD objects that follow tracked objects
+/// </summary>
+
+public class HudTracker : MonoBehaviour
+{
+    public List<GameObject> existing_trackers;
+    public GameObject tracker_parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tracker_parent = GameObject.Find("TrackerParent").gameObject;
+    }
+
+    public void EnableTracker()
+    {
+        //test if theres already a tracker for type, if not then create new tracker
+        GameObject new_tracker = Instantiate(Resources.Load("Prefabs/DetectorCircle")) as GameObject;
+        new_tracker.transform.parent = tracker_parent.transform;
+        existing_trackers.Add(new_tracker);
+
+        //add a type parameter to pass in to set name
+        new_tracker.GetComponent<HUDObject>().SetName("TestName");
+    }
+
+    public void UpdateTracker(int index, Vector3 position)
+    {
+        //move tracker to be over the objects postion
+    }
+
+    public void DisableTracker(int index)
+    {
+        existing_trackers[index].SetActive(false);
+    }
+}
